fc10b998470c0ae87265fbaaf1fae4c7416606ef
KwnxpN <138878357+KwnxpN@users.noreply.github.com>
2023-08-11 19:51:36 +0700

    new infla

--- PythonWeek5/Inflation.py
+++ PythonWeek5/Inflation.py
@@ -1,16 +1,9 @@
 '''inflation'''
 import math
-
-def round_down(n, decimals=0):
-    """lol"""
-    multiplier = 10 ** decimals
-    return math.floor(n * multiplier) / multiplier
-
 def inflate(money, years):
     '''find current value of money'''
     for _ in range(years):
         new_money = money + (money*0.0381)
-        new_money = round_down(new_money, 2)
         money = new_money
-    print(new_money)
+    print("%.2f"%(math.floor(money*100)/100.0))
 inflate(float(input()), int(input()))
